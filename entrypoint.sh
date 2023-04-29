bd19a3eea9e91f38e1e268eba58c719eafd5d5e6
Kroese <kroese@users.noreply.github.com>
2023-04-29 05:02:52 +0200

    Add support for body

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+INPUT_TITLE=${INPUT_TITLE//[[:blank:]]/}
+
 if [ -z "${INPUT_TAG}" ]; then
   INPUT_TAG="$(date +%Y%m%d%H%M%S)"
 fi
 
-hub release create -m "${INPUT_TITLE}" "${INPUT_TAG}" 
+if [ -z "${INPUT_BODY}" ]; then
+  INPUT_MSG="${INPUT_TITLE}"
+else
+  INPUT_MSG="${INPUT_TITLE} ${INPUT_BODY}"
+fi
+
+hub release create -m "${INPUT_MSG}" "${INPUT_TAG}" 
