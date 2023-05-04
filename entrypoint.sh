be1c7647125c789ff4f6f96005d6ebd6564d53d5
Kroese <kroese@users.noreply.github.com>
2023-05-04 10:16:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,16 @@ if [ -z "${INPUT_TAG}" ]; then
   INPUT_TAG="$(date +%Y%m%d%H%M%S)"
 fi
 
-if [ -z "${INPUT_BODY}" ]; then
-  hub release create -m "${INPUT_TITLE}" "${INPUT_TAG}" 
+if [ -z "${INPUT_COMMIT}" ]; then
+  if [ -z "${INPUT_BODY}" ]; then
+    hub release create -m "${INPUT_TITLE}" "${INPUT_TAG}" 
+  else
+    hub release create -m "${INPUT_TITLE}" -m "${INPUT_BODY}" "${INPUT_TAG}" 
+  fi
 else
-  hub release create -m "${INPUT_TITLE}" -m "${INPUT_BODY}" "${INPUT_TAG}" 
+  if [ -z "${INPUT_BODY}" ]; then
+    hub release create -t "${INPUT_COMMIT}" -m "${INPUT_TITLE}" "${INPUT_TAG}" 
+  else
+    hub release create -t "${INPUT_COMMIT}" -m "${INPUT_TITLE}" -m "${INPUT_BODY}" "${INPUT_TAG}" 
+  fi 
 fi
