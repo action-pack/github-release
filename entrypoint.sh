9edadf62eee8d9de87d4e95416ed5089c859d81f
Kroese <kroese@users.noreply.github.com>
2023-12-13 18:06:31 +0100

    fix: Check for existing release

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,26 @@ if [ -z "${INPUT_TAG}" ]; then
   INPUT_TAG="$(date +%Y%m%d%H%M%S)"
 fi
 
-{ gh release delete "${INPUT_TAG}" --cleanup-tag --yes 2> /dev/null; } || :
+{ RESULT=$(gh release view $INPUT_TAG 2>&1); } || :
+
+if [[ "$RESULT" != "release not found" ]]; then
+  echo "Release does already exists:"
+  echo $RESULT
+  echo "Performing delete..."
+  gh release delete "$INPUT_TAG" --cleanup-tag --yes
+  sleep 1
+fi
 
 if [ -z "${INPUT_COMMIT}" ]; then
   if [ -z "${INPUT_BODY}" ]; then
-    gh release create -t "${INPUT_TITLE}" "${INPUT_TAG}" --generate-notes
+    gh release create -t "$INPUT_TITLE" "$INPUT_TAG" --generate-notes
   else
-    gh release create -t "${INPUT_TITLE}" -n "${INPUT_BODY}" "${INPUT_TAG}" 
+    gh release create -t "$INPUT_TITLE" -n "$INPUT_BODY" "$INPUT_TAG" 
   fi
 else
-  if [ -z "${INPUT_BODY}" ]; then
-    gh release create --target "${INPUT_COMMIT}" -t "${INPUT_TITLE}" "${INPUT_TAG}" --generate-notes
+  if [ -z "$INPUT_BODY" ]; then
+    gh release create --target "$INPUT_COMMIT" -t "$INPUT_TITLE" "$INPUT_TAG" --generate-notes
   else
-    gh release create --target "${INPUT_COMMIT}" -t "${INPUT_TITLE}" -n "${INPUT_BODY}" "${INPUT_TAG}" 
+    gh release create --target "$INPUT_COMMIT" -t "$INPUT_TITLE" -n "$INPUT_BODY" "$INPUT_TAG" 
   fi 
 fi
