635745a27d603d67b0b8320a0aa68b793e2a08f2
Kroese <kroese@users.noreply.github.com>
2023-12-14 01:49:05 +0100

    fix: Detect existing release

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,14 @@ set -Eeuo pipefail
 INPUT_TITLE="$(echo "$INPUT_TITLE" | tr -d ' ')"
 [ -z "$INPUT_TAG" ] && INPUT_TAG="$(date +%Y%m%d%H%M%S)"
 
-{ result=$(gh release view "$INPUT_TAG"); rc=$?; } || :
+{ gh release view "$INPUT_TAG" >/dev/null 2>&1; rc=$?; } || :
 
 if (( rc == 0 )); then
   echo "Release $INPUT_TAG does already exists, will be overwritten..."
   gh release delete "$INPUT_TAG" --cleanup-tag --yes
   sleep 1
+else
+  echo $rc
 fi
 
 if [ -z "${INPUT_COMMIT}" ]; then
