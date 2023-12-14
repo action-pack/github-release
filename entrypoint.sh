3a89401694fd105b4e7c9f21c387bf6505428fa7
Kroese <kroese@users.noreply.github.com>
2023-12-14 02:51:38 +0100

    fix: Set token from input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "Setting token..."
+  export GITHUB_TOKEN="$INPUT_TOKEN"
+fi
+
 [ -z "$INPUT_TITLE" ] && INPUT_TITLE="Name"
 INPUT_TITLE="$(echo "$INPUT_TITLE" | tr -d ' ')"
 [ -z "$INPUT_TAG" ] && INPUT_TAG="$(date +%Y%m%d%H%M%S)"
