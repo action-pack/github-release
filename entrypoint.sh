0be15ba8fc740b8c9eee841c5d2d62dbcdbef7c5
Kroese <kroese@users.noreply.github.com>
2024-09-03 14:16:30 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,6 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
+#!/bin/bash
+echo ">> elgohr/Github-Release-Action@master is deprecated!"
+echo ">> Please switch to elgohr/Github-Release-Action@v4"
 
-if [ -z "${GITHUB_TOKEN:-}" ]; then
-  if [ -n "$INPUT_TOKEN" ]; then
-    export GITHUB_TOKEN="$INPUT_TOKEN"
-  else
-    echo "The token input is not set!" && exit 1
-  fi
-fi
-
-[ -z "$INPUT_TITLE" ] && INPUT_TITLE="Name"
-INPUT_TITLE="$(echo "$INPUT_TITLE" | tr -d ' ')"
-[ -z "$INPUT_TAG" ] && INPUT_TAG="$(date +%Y%m%d%H%M%S)"
-
-{ gh release view "$INPUT_TAG" >/dev/null 2>&1; rc=$?; } || :
-
-if (( rc == 0 )); then
-  echo "Release tag $INPUT_TAG does already exists, it will be replaced..."
-  gh release delete "$INPUT_TAG" --cleanup-tag --yes
-  sleep 3
-fi
-
-if [ -z "$INPUT_COMMIT" ]; then
-  if [ -z "$INPUT_BODY" ]; then
-    gh release create -t "$INPUT_TITLE" "$INPUT_TAG" --generate-notes
-  else
-    gh release create -t "$INPUT_TITLE" -n "$INPUT_BODY" "$INPUT_TAG" 
-  fi
-else
-  if [ -z "$INPUT_BODY" ]; then
-    gh release create --target "$INPUT_COMMIT" -t "$INPUT_TITLE" "$INPUT_TAG" --generate-notes
-  else
-    gh release create --target "$INPUT_COMMIT" -t "$INPUT_TITLE" -n "$INPUT_BODY" "$INPUT_TAG" 
-  fi 
-fi
+MESSAGE=$*
+hub release create -m ${MESSAGE} $(date +%Y%m%d%H%M%S)
